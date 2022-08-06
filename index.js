a3fb65ca620ee7059010af1289eeb9e171cf5c40
Jonathan <74022542+KingApe714@users.noreply.github.com>
2022-08-06 06:41:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Encapsulation: Encapsulation means information hiding. It's about hiding as much as possible of the object's internal parts and exposing a minimal public interface.
+
+//It is important to use OOP in order to DRY up our code and make it easy to track where certain actions are taking place. 
+
+//If we wrote code without using classes it would mean that we would have to repeat the same code in different places making it more difficult to track where certain things are taking place.
+
+//We can use encapsulation to put the details behind an object's data and behaviors
+
+//Understanding encapsulation can help developers break down large, complex problems into objects and place implementation details of a feature hidden within an object.
+
+//Lets create a person object
+
+//Three things to take note of when creating a an instance of a class
+
+// 1) A new empty object is created (i.e. {})
+// 2) The constructor method is called and this is bound to the new object
+// 3) The new object is returned after the constructor method has completed
+
+// A method is a function that is part of the class. It performs an action (verb) for the class.
+
+// Methods are typically invoked on a given instance of the class stored in a variable; for this reason, they are also called instance methods.
+
+//Lets create and instance and invoke the method outside of the class
+
+//Now lets use the instanceOf operator to check an object's type
+
+//Notice that the word object gets used a lot to mean different things. Depending on the language and the topic one is discussing, the word 'object' could mean something completely different.
+
+//POJO: Plain Old Javascript Object
+//JSON: JavaScript Object Notation
